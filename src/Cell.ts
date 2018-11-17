52bab4ead878957718d042cd61222e525cc8a7c9
Rosana Ruiz <rosanarufer@gmail.com>
2018-11-17 15:34:11 +0000

    Green: A cell should know the location of its neighbours

---
+++ src/Cell.ts
@@ -0,0 +1,35 @@
+export class Location {
+    public x: number;
+    public y: number;
+    constructor(x: number, y: number){
+        this.x = x;
+        this.y = y;
+    }
+}
+export class Cell {
+
+    private alive: boolean;
+    private location: Location;
+
+    constructor(isAlive: boolean, location: Location = new Location(0,0)){
+        this.alive = isAlive;
+        this.location = location;
+    }
+
+    public isAlive():boolean {
+        return this.alive;
+    }
+    public getLocation():Location {
+        return this.location;
+    }
+    public getNeighbors():Location[]{
+        const x = this.location.x;
+        const y = this.location.y;
+        return [
+            new Location(x-1,y-1), new Location(x, y-1),
+            new Location(x+1, y-1), new Location(x-1,y),
+            new Location(x+1, y), new Location(x-1, y+1),
+            new Location(x, y+1), new Location(x+1, y+1)
+        ];
+    }
+}
